9fc740b9edb1f8b16048b913c89df14f39ec848a
darren2812 <127184690+darren2812@users.noreply.github.com>
2025-08-21 10:47:08 -0700

    Add files via upload

---
+++ LargestOfThree.java
@@ -0,0 +1,37 @@
+// IntComparison.java
+// dD 08/21/25
+
+// importing the Scanner
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args){
+        // asks the user how many integers they want to compare
+        System.out.println("How many integers do you want to compare? ");
+        // creates the scanner object
+        Scanner input = new Scanner(System.in);
+        int numbersToCompare = input.nextInt();
+        // creates an array of a specified size according how many integers are compared
+        int[] numbers = new int[numbersToCompare];
+        // iterates to ask for user input
+        for (int i = 0; i < numbersToCompare; i++){
+            System.out.print("Number " + (i + 1) + ": ");
+            numbers[i] = input.nextInt();
+        }
+        // prints output message and calls out the compareInts function
+        System.out.println("The largest integer is " + compareInts(numbers, numbersToCompare));
+    }
+
+    static int compareInts(int[] nums, int size){
+        // sets the largest integer to the first element in the array
+        int largest = nums[0];
+        // iterates through the array and replaces largest if the next element in the array is larger
+        for (int i = 0; i < size-1; i++){
+            if (nums[i] < nums[i+1]){
+                largest = nums[i+1];
+            }
+        }
+        // returns the largest number
+        return largest;
+    }
+}
